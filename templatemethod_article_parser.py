e1d160e16cd21459c9094f588284520a2dedaa53
Maxim Yurevich <max.y@tut.by>
2022-01-18 22:42:53 +0300

    initial commit

---
+++ templatemethod_article_parser.py
@@ -0,0 +1,37 @@
+"""Template method article parser"""
+from abc import ABC, abstractmethod
+import logging
+from newspaper import Article
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class AbstractArticleParser(ABC):
+    """Abstract article parser"""
+    article: Article
+
+    """Abstract Article Parser Class
+
+    Args:
+        ABC (class): Helper class
+    """
+    def __init__(self, url: str) -> None:
+        self.url = url
+
+    async def get_article(self):
+        """Template method"""
+        await self.load_article()
+        await self.read_article()
+
+    async def load_article(self) -> None:
+        """Load and parse article"""
+        article = Article(self.url)
+        article.download()
+        article.parse()
+        self.article = article
+
+    @abstractmethod
+    async def read_article(self):
+        """Read article and return content"""
+        ...
